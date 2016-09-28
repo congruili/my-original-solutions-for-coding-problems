349fefefd7c16e3ed09e5fdc7d55e14039704715
lic10 <lic10@rpi.edu>
2016-09-28 12:49:16 -0400

    add new files

--- 403.java
+++ 403.java
@@ -1,25 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public int sumOfLeftLeaves(TreeNode root) {
-        int rst = 0;
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 0;
-        if (root.left != null) {
-            if (root.left.left == null && root.left.right == null) rst += root.left.val;
-            else rst += sumOfLeftLeaves(root.left);
+    public boolean canCross(int[] stones) {
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        for (int i: stones) map.put(i, new HashSet<Integer>());
+        
+        if (map.containsKey(1)) map.get(1).add(1);
+        else return false;
+        
+        int len = stones.length;
+        
+        for (int i = 1; i < len; ++i) {
+            int curt = stones[i];
+            HashSet<Integer> set = map.get(curt);
+            
+            for (int j: set) {
+                int stepSize = j - 1;
+                for (int k = 0; k < 3; ++k) {
+                    if (stepSize != 0 && map.containsKey(curt + stepSize)) map.get(curt + stepSize).add(stepSize);
+                    stepSize++;
+                }
+            }
         }
         
-        rst += sumOfLeftLeaves(root.right);
-        
-        return rst;
-        
+        return !map.get(stones[len - 1]).isEmpty();
     }
 }
